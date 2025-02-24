de14bddf32de9dfbecec7c411c830c45ecfc7cc1
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-02-24 16:04:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,23 +30,6 @@ else:
 
 
 
-def get_redirected_url(modified_url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-    }
-
-    try:
-        response = requests.get(modified_url, headers=headers, allow_redirects=True)
-
-        
-        final_url = response.url
-
-        if "scontent.cdninstagram.com" in final_url:
-            return final_url
-        return None
-
-    except Exception as e:
-        return None
 
 @app.route("/")
 def home():
@@ -89,31 +72,6 @@ def home():
     """
 
 
-@app.route('/convert', methods=['GET'])
-def convert_reel():
-    url = request.args.get('url')
-
-    if not url:
-        return jsonify({"error": "Instagram Reels, TV, or Post URL is required"}), 400
-
-    match = re.search(r'/(reel|tv|p)/([^/?]+)', url)
-    
-    if not match:
-        return jsonify({"error": "Invalid Instagram URL format"}), 400
-
-    content_id = match.group(2)  # Extract only the ID
-    modified_url = f"https://www.ddinstagram.com/grid/{content_id}"  # Corrected format
-
-    # Get the final redirected URL (direct video link)
-    video_url = get_redirected_url(modified_url)
-
-    if not video_url:
-        return jsonify({"error": f"Failed to {content_id} fetch video link"}), 500
-
-    return jsonify({
-        "dwn_url": video_url
-    })
-    
 @app.route("/profile", methods=["GET"])
 def get_profile():
     username = request.args.get("username")
